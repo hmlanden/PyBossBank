e777e4edc9e5ad94fdd5a7a77afb6d9420479e8c
heidi_laptop_home <hmlanden@gmail.com>
2018-02-04 13:23:30 -0800

    ouput results file

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -3,7 +3,14 @@ import os
 import pandas as pd
 import locale
 
-# Step One: Get all my data into a clean data frame that I can manipulate
+# ----------------------------------------------------------------------
+# Step 0: Give instructions to the user
+# ----------------------------------------------------------------------
+print()
+
+# ----------------------------------------------------------------------
+# Step 1: Get all my data into a clean data frame that I can manipulate
+# ----------------------------------------------------------------------
 
 # Get paths for the file that we're going to be using
 revenue_file = input("Input the path name of the file that you want to analyze! FYI, you're currently at Documents/Github/UCBerkeley_HomeworkAssignment3/ ")
@@ -19,8 +26,9 @@ revenue_df = pd.read_csv(revenue_file)
 # - The greatest increase in revenue (date and amount) over the entire period
 # - The greatest decrease in revenue (date and amount) over the entire period
 
-
+# ----------------------------------------------------------------------
 # Step 2: Perform analyses
+# ----------------------------------------------------------------------
 
 # Total number of months
 count_months = len(revenue_df['Date'].unique())
@@ -47,11 +55,15 @@ greatest_decrease_date = revenue_reindex_df.loc[greatest_decrease_amount,'Date']
 # set currency 
 locale.setlocale( locale.LC_ALL, 'en_US' )
 
-# Step 3: Format and return results
+# ----------------------------------------------------------------------
+# Step 3: Format and print results in terminal
+# ----------------------------------------------------------------------
 
 # print header
 print("Financial Analysis")
 print('-' * 60)
+
+# print results
 print("Time Period: {} months".format(count_months))
 print("Total Revenue: "+ str(locale.currency(total_revenue,grouping=True)))
 print("Average Revenue Change: " + str(locale.currency(average_revenue_change,grouping=True)))
@@ -61,4 +73,29 @@ print("Greatest Decrease in Revenue: "+ str(locale.currency(greatest_decrease_am
       " (" + greatest_decrease_date + ")")
 print('-' * 60)
 
+# print close
 print("The results of this analysis can be found in the text file named analysis_{}.txt.".format(revenue_file))
+
+
+# ----------------------------------------------------------------------
+# Step 4: Create and write results file
+# ----------------------------------------------------------------------
+
+# open the output file
+revenue_file_split = revenue_file.split("/")
+revenue_file_remove_csv = str(revenue_file_split[1]).split(".")
+output_filename = str("analysis_" + str(revenue_file_remove_csv[0]) + ".txt")
+results_file = open(output_filename,'w+')
+
+# write to file
+results_file.write("Financial Analysis \n")
+results_file.write('-' * 60 + "\n")
+results_file.write("Time Period: {} months  \n".format(count_months))
+results_file.write("Total Revenue: "+ str(locale.currency(total_revenue,grouping=True)) +  "\n")
+results_file.write("Average Revenue Change: " + str(locale.currency(average_revenue_change,grouping=True)) +  "\n")
+results_file.write("Greatest Increase in Revenue: "+ str(locale.currency(greatest_increase_amount,grouping=True)) + \
+                  " (" + greatest_increase_date + ")" +  "\n")
+results_file.write("Greatest Decrease in Revenue: "+ str(locale.currency(greatest_decrease_amount,grouping=True)) + \
+                  " (" + greatest_decrease_date + ")" +  "\n")
+results_file.write('-' * 60)
+results_file.close()
